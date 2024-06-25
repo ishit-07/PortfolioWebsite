817a13f93c81c36bcd7b981e6b265776ba591d36
ishit-07 <ishitverma2602@gmail.com>
2024-06-25 19:45:19 +0530

    updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Ishit Verma
-            </h1>
+            </motion.h1>
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               FrontEnd Developer
             </span>
