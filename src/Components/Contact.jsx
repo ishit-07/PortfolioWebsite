36a3bb0858ccd563ac2718ccbcbdc56ceaa19fb0
ishit-07 <ishitverma2602@gmail.com>
2024-06-30 23:13:58 +0530

    completed

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,14 +1,29 @@
 import { motion } from "framer-motion";
 
 const Contact = () => {
-    return (  
-        <div className="border-b border-neutral-900 pb-20">
-            <h1 className="my-10 text-xl text-center">Get in Touch</h1>
-            <div className="text-center tracking-tighter">
-                <a href="#" className="my-4 font-light">ishitverm2602@gmail.com</a>
-            </div>
-        </div>
-    );
-}
- 
+  return (
+    <div className="border-b border-neutral-900 pb-20">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-xl text-center"
+      >
+        Get in Touch
+      </motion.h1>
+      <div className="text-center tracking-tighter">
+        <motion.a
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          href="#"
+          className="my-4 font-light"
+        >
+          ishitverm2602@gmail.com
+        </motion.a>
+      </div>
+    </div>
+  );
+};
+
 export default Contact;
