1f70868dfeb47b73c0609b27ef8ee39ed348eef0
ishit-07 <ishitverma2602@gmail.com>
2024-07-19 23:12:02 +0530

    completed

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,25 +3,9 @@ import Weather from "../assets/Weather.png";
 import tictactoe from "../assets/TicTacToe.png";
 import Asus from "../assets/AsusROG.png";
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion
-              for creating efficient and user-friendly web applications. With 5
-              years of professional experience, I have worked with a variety of
-              technologies, including React, Next.js, Node.js, MySQL,
-              PostgreSQL, and MongoDB. My journey in web development began with
-              a deep curiosity for how things work, and it has evolved into a
-              career where I continuously strive to learn and adapt to new
-              challenges. I thrive in collaborative environments and enjoy
-              solving complex problems to deliver high-quality solutions.
-              Outside of coding, I enjoy staying active, exploring new
-              technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. Having recently completed my studies, I have gained experience working with technologies like React, Node.js, and Express.js through various projects and internship. My journey in web development began with a deep curiosity for how things work, and it has evolved into a pursuit where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
-export const HERO_TEXT = `I am a passionate full stack developer with a knack for crafting
-              robust and scalable web applications. With 5 years of hands-on
-              experience, I have honed my skills in front-end technologies like
-              React and Next.js, as well as back-end technologies like Node.js,
-              MySQL, PostgreSQL, and MongoDB. My goal is to leverage my
-              expertise to create innovative solutions that drive business
-              growth and deliver exceptional user experiences.`;
+export const HERO_TEXT = `I am a passionate full stack developer eager to craft robust and scalable web applications. Having recently completed my studies, I have built a solid foundation in front-end technologies like React, as well as back-end technologies like Node.js and Express.js through various projects and internship. My goal is to leverage my knowledge and enthusiasm to create innovative solutions that drive business growth and deliver exceptional user experiences..`;
 
 export const EXPERIENCE = [
   {
