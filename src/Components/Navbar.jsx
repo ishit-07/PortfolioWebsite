62a797a2b235a94ea3306bff34f654b4438954e3
Ishit Verma <ishitverma2602@gmail.com>
2025-04-03 14:33:35 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import logo from "../assets/logo.png"
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+// import { FaSquareXTwitter } from "react-icons/fa6";
 
 
 const Navbar = () => {
@@ -12,8 +12,8 @@ const Navbar = () => {
             <div className="m-8 flex items-center gap-4 text-2xl justify-center">
                 <a href="https://www.linkedin.com/in/ishit-verma/"><FaLinkedin  /></a>
                 <a href="https://github.com/ishit-07"><FaGithub /></a>
-                <a href="https://x.com/ishit__07"><FaSquareXTwitter /></a>
-                <a href="https://www.instagram.com/_ishit_07_/"><FaInstagram /></a>
+                {/* <a href="https://x.com/ishit__07"><FaSquareXTwitter /></a>
+                <a href="https://www.instagram.com/_ishit_07_/"><FaInstagram /></a> */}
             </div>
         </nav>
     );
